73dd0784aafb53536621b18667fff5f605729a29
pjleb90 <pjleb90@gmail.com>
2023-07-02 14:17:00 -0400

    created a reusable Button component

--- components/Header.tsx
+++ components/Header.tsx
@@ -102,11 +102,23 @@ const Header: React.FC<HeaderProps> = ({
             <div className="flex justify-between items-center gap-x-4">
               <>
                 <div>
-                  <Button/>
+                  <Button
+                    onClick={() => {}}
+                    className="bg-transparent text-neutral-300 font-medium">
+                      Sign up
+                  </Button>
+                </div>
+                <div>
+                  <Button
+                    onClick={() => {}}
+                    className="bg-white px-6 py-2">
+                      Log In
+                  </Button>
                 </div>
               </>
             </div>
           </div>
+          {children}
         </div>
       );
 }
