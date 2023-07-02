e77571fd0eb36cad1f280e2fba0a456a86d09188
pjleb90 <pjleb90@gmail.com>
2023-07-02 13:25:04 -0400

    update sidebar with library component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,6 +8,7 @@ import { BiSearch } from "react-icons/bi";
 
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -63,7 +64,7 @@ const Sidebar: React.FC<SidebarProps> = ({
                     </div>
                 </Box>
                 <Box className="overflow-y-auto h-full">
-                    Song Library
+                    <Library />
                 </Box>
             </div>
             <main className="h-full flex-1 overflow-y-auto py-2">
