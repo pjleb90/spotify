282d3242a9d4b11692e6a0a6a651a332e63c7ca2
pjleb90 <pjleb90@gmail.com>
2023-07-09 12:47:08 -0400

    added log in/out functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Sidebar from '@/components/Sidebar'
 import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -21,14 +22,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <SupabaseProvider>
-          <UserProvider>
-            <ModalProvider />
-            <Sidebar>
-              {children}
-            </Sidebar>
-          </UserProvider>
-        </SupabaseProvider>
+        <ToasterProvider />
+          <SupabaseProvider>
+            <UserProvider>
+              <ModalProvider />
+              <Sidebar>
+                {children}
+              </Sidebar>
+            </UserProvider>
+          </SupabaseProvider>
       </body>
     </html>
   )
