80cdc6e0943dbc185e71da6d3d975597fc29eacd
pjleb90 <pjleb90@gmail.com>
2023-07-16 14:33:45 -0400

    added like songs functionality and new liked songs page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -8,6 +8,8 @@ interface SearchProps {
     searchParams: { title: string }
 };
 
+export const revalidate = 0;
+
 const Search = async ({ searchParams }: SearchProps) => {
     const songs = await getSongsByTitle(searchParams.title);
 
